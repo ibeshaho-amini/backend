5c1175c3b11cfee58f1d6a90b427394edc15e907
amini-ibeshaho <aminibeshaho@gmail.com>
2024-09-05 08:25:31 +0200

    mongoose

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Connect to MongoDB and start the server
 if (process.env.NODE_ENV !== 'test') {
-    mongoose.connect("mongodb://localhost:27017/blog")
+    mongoose.connect("mongodb+srv://aminibeshaho:Ibeshaho%40123@cluster0.vrp96.mongodb.net/blogs_db?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         app.listen(5000, () => {
             console.log("Server has started on port 5000!");
