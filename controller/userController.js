d966990f132d466861055a343229013f659642a7
amini-ibeshaho <aminibeshaho@gmail.com>
2024-09-25 16:11:56 +0200

    ....

--- controller/userController.js
+++ controller/userController.js
@@ -54,7 +54,7 @@ exports.loginUser = async (req, res) => {
         // res.send({ token: `Bearer ${token}` });
         res.send({ 
             token: `Bearer ${token}`,
-            redirectTo: 'portfolio/adminiPanel.html' 
+            redirectTo: './adminiPanel.html' 
         });
         
     } catch (error) {
